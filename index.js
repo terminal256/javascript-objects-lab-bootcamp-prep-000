9889915db8d2016e0675580c7ad8738a4089362d
Gregorio Manabat <gregoriomanabat@gmail.com>
2018-12-17 05:14:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
+  return object;
 }
 function deleteFromObjectByKey(object, key){
   var copy = copyObj(object);
@@ -16,5 +17,6 @@ function deleteFromObjectByKey(object, key){
   return copy;
 }
 function destructivelyDeleteFromObjectByKey(object, key){
-  delete object[key]
+  delete object[key];
+  return object;
 }
